4204d142cfe755b0f54e70b412f304b31b478698
eagleDog <ethandragoin@gmail.com>
2022-02-10 13:31:12 -0700

    wiring

--- lib/wads/themes.rb
+++ lib/wads/themes.rb
@@ -4,6 +4,42 @@
 module Wads
 
     #
+    # An instance of GuiTheme directs wads widgets as to what colors and fonts
+    # should be used. This accomplishes two goals: one, we don't need to constantly
+    # pass around these instances. They can be globally accessed using WadsConfig.
+    # It also makes it easy to change the look and feel of your application.
+    # 
+    class GuiTheme 
+        attr_accessor :text_color
+        attr_accessor :graphic_elements_color
+        attr_accessor :border_color
+        attr_accessor :background_color
+        attr_accessor :selection_color
+        attr_accessor :use_icons
+        attr_accessor :font
+        attr_accessor :font_large
+
+        def initialize(text, graphics, border, background, selection, use_icons, font, font_large) 
+            @text_color = text 
+            @graphic_elements_color = graphics
+            @border_color = border 
+            @background_color = background 
+            @selection_color = selection 
+            @use_icons = use_icons
+            @font = font
+            @font_large = font_large
+        end
+
+        def pixel_width_for_string(str)
+            @font.text_width(str)
+        end
+
+        def pixel_width_for_large_font(str)
+            @font_large.text_width(str)
+        end
+    end
+
+    #
     # Theme with black text on a white background
     #
     class WadsBrightTheme < GuiTheme 
